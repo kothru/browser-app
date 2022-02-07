29b9edee47ff3850abea049101699e0e4ec788f4
kothru <1894683+kothru@users.noreply.github.com>
2022-02-07 21:47:41 +0900

    add listener

--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,15 @@
+import { EventListener } from './EventListener'
 class Application {
   start() {
-    console.log('hello world')
+    const eventListener = new EventListener()
+    const button = document.getElementById('deleteAllDoneTask')
+    if (!button) return
+
+    eventListener.add(
+      'sample', 'click', button, () => {
+        alert('clicked')
+      }
+    )
   }
 }
 window.addEventListener('load', () => {
