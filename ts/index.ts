005f8e48277db41c60e6fc44e502a67b9b300650
kothru <1894683+kothru@users.noreply.github.com>
2022-02-07 21:59:06 +0900

    change start

--- ts/index.ts
+++ ts/index.ts
@@ -1,16 +1,13 @@
 import { EventListener } from './EventListener'
 class Application {
+  private readonly eventListener = new EventListener()
   start() {
-    const eventListener = new EventListener()
-    const button = document.getElementById('deleteAllDoneTask')
-    if (!button) return
-
-    eventListener.add(
-      'sample', 'click', button, () => {
-        alert('clicked')
-      }
-    )
-    eventListener.remove('sample')
+    const createForm = document.getElementById('createForm') as HTMLElement
+    this.eventListener.add('submit-handler', 'submit', createForm, this.handleSubmit)
+  }
+  private handleSubmit = (e: Event) => {
+    e.preventDefault()
+    console.log('submitted')
   }
 }
 window.addEventListener('load', () => {
