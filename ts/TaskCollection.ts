06b9a5f891bfc0dbfabeb79bb0675f33adf4a3ef
kothru <1894683+kothru@users.noreply.github.com>
2022-02-09 09:20:47 +0900

    add storage proc

--- ts/TaskCollection.ts
+++ ts/TaskCollection.ts
@@ -1,12 +1,19 @@
 import { Status, Task } from "./Task";
-
+const STORAGE_KEY = 'TASKS'
 export class TaskCollection {
+  private readonly storage
   private tasks: Task[] = []
+  constructor() {
+    this.storage = localStorage
+    this.getStoredTasks()
+  }
   add(task: Task) {
     this.tasks.push(task)
+    this.updateStorage()
   }
   delete(task: Task) {
     this.tasks = this.tasks.filter(({ id }) => id !== task.id)
+    this.updateStorage()
   }
   find(id: string) {
     return this.tasks.find((task) => task.id === id)
@@ -19,5 +26,15 @@ export class TaskCollection {
   }
   filter(filterStatus: Status) {
     return this.tasks.filter(({ status }) => status === filterStatus)
+  }
+  private updateStorage() {
+    this.storage.setItem(STORAGE_KEY, JSON.stringify(this.tasks))
+  }
+  private getStoredTasks(): Task[] {
+    const jsonString = this.storage.getItem(STORAGE_KEY)
+    if (!jsonString) return []
+
+    console.log(jsonString)
+    return []
   }
 }
