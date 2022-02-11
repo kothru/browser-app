93bb734a9807639db14a4353844348a565660485
kothru <1894683+kothru@users.noreply.github.com>
2022-02-11 15:07:34 +0900

    id automate

--- ts/index.ts
+++ ts/index.ts
@@ -16,10 +16,10 @@ class Application {
     const deleteAllDoneTaskButton = document.getElementById('deleteAllDoneTask') as HTMLElement
 
     taskItems.forEach(({ task, deleteButtonEl }) => {
-      this.eventListener.add(task.id, 'click', deleteButtonEl, () => this.handleClickDeleteTask(task))
+      this.eventListener.add('click', deleteButtonEl, () => this.handleClickDeleteTask(task), task.id)
     })
-    this.eventListener.add('submit-handler', 'submit', createForm, this.handleSubmit)
-    this.eventListener.add('click-handler', 'click', deleteAllDoneTaskButton, this.handleClickDeleteAllDoneTasks)
+    this.eventListener.add('submit', createForm, this.handleSubmit)
+    this.eventListener.add('click', deleteAllDoneTaskButton, this.handleClickDeleteAllDoneTasks)
     this.taskRenderer.subscribeDragAndDrop(this.handleDropAndDrop)
   }
 
@@ -32,7 +32,7 @@ class Application {
     this.taskCollection.add(task)
     const { deleteButtonEl } = this.taskRenderer.append(task)
     this.eventListener.add(
-      task.id, 'click', deleteButtonEl, () => this.handleClickDeleteTask(task)
+      'click', deleteButtonEl, () => this.handleClickDeleteTask(task), task.id
     )
 
     titleInput.value = ''
